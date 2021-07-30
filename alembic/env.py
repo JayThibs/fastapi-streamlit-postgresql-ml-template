6bc27e651fc9d83ab51acf02a220c2c82451dcc7
JayThibs <thibo.jacques@gmail.com>
2021-07-30 13:39:47 -0300

    Updated alembic env variables.

--- alembic/env.py
+++ alembic/env.py
@@ -50,7 +50,7 @@ def run_migrations_offline():
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = get_url()
     context.configure(
         url=url,
         target_metadata=target_metadata,
