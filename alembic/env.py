468caaecb6f1dfa6177fa779e75cbe0704f16691
JayThibs <thibo.jacques@gmail.com>
2021-07-30 13:41:44 -0300

    Cleaned up imports.

--- alembic/env.py
+++ alembic/env.py
@@ -1,13 +1,9 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
 
 import os
 from sqlalchemy import create_engine
-from logging.config import fileConfig
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
