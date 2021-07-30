8abad5ec036d2485bf02307774f17631ee57fd00
JayThibs <thibo.jacques@gmail.com>
2021-07-30 00:26:58 -0300

    Created database file to get db. Added env variable.

---
+++ src/database.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DBPW = os.getenv("DBPW")
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:{DBPW}@localhost/banknote"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    except:
+        db.close()
